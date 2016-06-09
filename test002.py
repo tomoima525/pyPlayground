294bf4f392e4a76deb88555d9ccbc26215f08b21
tomoima525 <tomoima525@gmail.com>
2016-06-09 08:44:12 -0700

    Comprehension

--- test002.py
+++ test002.py
@@ -1,3 +1,15 @@
 num1=[98,43, -12,5,24,43]
 num2=sorted(num1)
 print(num2)
+
+num3=set(num1)
+print(num3)
+
+lst=[]
+
+for num in range(0,21,2):
+    lst.append(num**2)
+print(lst)
+
+lst2=[num**3 for num in range(0,21,2)]
+print(lst2)
